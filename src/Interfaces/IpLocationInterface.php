0e52162e1e9072813e31a220f9ebd35b66852cbc
timeshow <397975896@qq.com>
2021-10-12 15:53:22 +0800

    first commit

---
+++ src/Interfaces/IpLocationInterface.php
@@ -0,0 +1,62 @@
+<?php
+namespace TimeShow\IpLocation\Interfaces;
+
+interface IpLocationInterface {
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return mixed
+     */
+    public function getLocation($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getArea($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getCountry($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getProvince($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getCity($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getCounty($ip = '');
+
+    /**
+     *
+     * @access Public
+     * @param string $ip
+     * @return string
+     */
+    public function getISP($ip = '');
+
+}
